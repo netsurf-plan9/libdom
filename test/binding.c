2df119603f5a560ffb6cafe85f6377076276a34a
John Mark Bell <jmb@netsurf-browser.org>
2007-07-15 22:09:59 +0000

    Implement TestObject class and other test utilities (testutils.h) Rudimentary test of libxml binding, so I can see what's needed
    svn path=/trunk/dom/; revision=3419

---
+++ test/binding.c
@@ -0,0 +1,29 @@
+#include <dom/dom.h>
+#include "testutils.h"
+
+int main(int argc, char **argv)
+{
+	struct dom_document *doc;
+	struct dom_element *element;
+	struct dom_string *elementName;
+	dom_exception err;
+	TestObject *staff;
+
+	staff = test_object_create(argc, argv, "staff", false);
+	assert(staff != NULL);
+
+	doc = test_object_get_doc(staff);
+	assert(doc != NULL);
+
+	err = dom_document_get_element(doc, &element);
+	assert(err == DOM_NO_ERR);
+	assert(element != NULL);
+
+	err = dom_element_get_tag_name(element, &elementName);
+	assert(err == DOM_NO_ERR);
+	assert(elementName != NULL);
+
+	printf("PASS\n");
+
+	return 0;
+}
