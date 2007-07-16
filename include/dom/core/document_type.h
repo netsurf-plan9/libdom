d786335f46d7abea40352c7c0c39e0cd08e431b6
John Mark Bell <jmb@netsurf-browser.org>
2007-07-16 22:03:54 +0000

    Stub out remainder of DocumentType API. Minor other changes.
    svn path=/trunk/dom/; revision=3424

--- include/dom/core/document_type.h
+++ include/dom/core/document_type.h
@@ -3,19 +3,34 @@
  * Licensed under the MIT License,
  *                http://www.opensource.org/licenses/mit-license.php
  * Copyright 2007 John-Mark Bell <jmb@netsurf-browser.org>
+ * Copyright 2007 James Shaw <EMAIL_ADDRESS>
  */
 
 #ifndef dom_core_document_type_h_
 #define dom_core_document_type_h_
 
-#include <stdbool.h>
-
 #include <dom/core/exceptions.h>
 
-struct dom_string;
 struct dom_document_type;
+struct dom_namednodemap;
+struct dom_string;
 
-dom_exception dom_document_type_get_name(struct dom_document_type *docType,
+dom_exception dom_document_type_get_name(struct dom_document_type *doc_type,
+		struct dom_string **result);
+dom_exception dom_document_type_get_entities(
+		struct dom_document_type *doc_type,
+		struct dom_namednodemap **result);
+dom_exception dom_document_type_get_notations(
+		struct dom_document_type *doc_type,
+		struct dom_namednodemap **result);
+dom_exception dom_document_type_get_public_id(
+		struct dom_document_type *doc_type,
+		struct dom_string **result);
+dom_exception dom_document_type_get_system_id(
+		struct dom_document_type *doc_type,
+		struct dom_string **result);
+dom_exception dom_document_type_get_internal_subset(
+		struct dom_document_type *doc_type,
 		struct dom_string **result);
 
 #endif
--- src/core/document_type.c
+++ src/core/document_type.c
@@ -3,17 +3,145 @@
  * Licensed under the MIT License,
  *                http://www.opensource.org/licenses/mit-license.php
  * Copyright 2007 John-Mark Bell <jmb@netsurf-browser.org>
+ * Copyright 2007 James Shaw <EMAIL_ADDRESS>
  */
 
-#include <dom/functypes.h>
 #include <dom/core/document_type.h>
+
+#include "core/node.h"
 #include "utils/utils.h"
 
-dom_exception dom_document_type_get_name(struct dom_document_type *docType,
+/**
+ * DOM DocumentType node
+ */
+struct dom_document_type {
+	struct dom_node base;		/**< Base node */
+
+	/** \todo other members */
+};
+
+/**
+ * Retrieve a document type's name
+ *
+ * \param doc_type  Document type to retrieve name from
+ * \param result    Pointer to location to receive result
+ * \return DOM_NO_ERR.
+ *
+ * The returned string will have its reference count increased. It is
+ * the responsibility of the caller to unref the string once it has
+ * finished with it.
+ */
+dom_exception dom_document_type_get_name(struct dom_document_type *doc_type,
 		struct dom_string **result)
 {
-	UNUSED(docType);
+	UNUSED(doc_type);
 	UNUSED(result);
 
 	return DOM_NOT_SUPPORTED_ERR;
 }
+
+/**
+ * Retrieve a document type's entities
+ *
+ * \param doc_type  Document type to retrieve entities from
+ * \param result    Pointer to location to receive result
+ * \return DOM_NO_ERR.
+ *
+ * The returned map will have its reference count increased. It is
+ * the responsibility of the caller to unref the map once it has
+ * finished with it.
+ */
+dom_exception dom_document_type_get_entities(
+		struct dom_document_type *doc_type,
+		struct dom_namednodemap **result)
+{
+	UNUSED(doc_type);
+	UNUSED(result);
+
+	return DOM_NOT_SUPPORTED_ERR;
+}
+
+/**
+ * Retrieve a document type's notations
+ *
+ * \param doc_type  Document type to retrieve notations from
+ * \param result    Pointer to location to receive result
+ * \return DOM_NO_ERR.
+ *
+ * The returned map will have its reference count increased. It is
+ * the responsibility of the caller to unref the map once it has
+ * finished with it.
+ */
+dom_exception dom_document_type_get_notations(
+		struct dom_document_type *doc_type,
+		struct dom_namednodemap **result)
+{
+	UNUSED(doc_type);
+	UNUSED(result);
+
+	return DOM_NOT_SUPPORTED_ERR;
+}
+
+/**
+ * Retrieve a document type's public id
+ *
+ * \param doc_type  Document type to retrieve public id from
+ * \param result    Pointer to location to receive result
+ * \return DOM_NO_ERR.
+ *
+ * The returned string will have its reference count increased. It is
+ * the responsibility of the caller to unref the string once it has
+ * finished with it.
+ */
+dom_exception dom_document_type_get_public_id(
+		struct dom_document_type *doc_type,
+		struct dom_string **result)
+{
+	UNUSED(doc_type);
+	UNUSED(result);
+
+	return DOM_NOT_SUPPORTED_ERR;
+}
+
+/**
+ * Retrieve a document type's system id
+ *
+ * \param doc_type  Document type to retrieve system id from
+ * \param result    Pointer to location to receive result
+ * \return DOM_NO_ERR.
+ *
+ * The returned string will have its reference count increased. It is
+ * the responsibility of the caller to unref the string once it has
+ * finished with it.
+ */
+dom_exception dom_document_type_get_system_id(
+		struct dom_document_type *doc_type,
+		struct dom_string **result)
+{
+	UNUSED(doc_type);
+	UNUSED(result);
+
+	return DOM_NOT_SUPPORTED_ERR;
+}
+
+/**
+ * Retrieve a document type's internal subset
+ *
+ * \param doc_type  Document type to retrieve internal subset from
+ * \param result    Pointer to location to receive result
+ * \return DOM_NO_ERR.
+ *
+ * The returned string will have its reference count increased. It is
+ * the responsibility of the caller to unref the string once it has
+ * finished with it.
+ */
+dom_exception dom_document_type_get_internal_subset(
+		struct dom_document_type *doc_type,
+		struct dom_string **result)
+{
+	UNUSED(doc_type);
+	UNUSED(result);
+
+	return DOM_NOT_SUPPORTED_ERR;
+}
+
