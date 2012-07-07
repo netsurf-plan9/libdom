b2d47100032594f73c62b3e1a987c8286b3d0411
Daniel Silverstone <dsilvers@digital-scurf.org>
2012-07-07 12:56:50 +0100

    Add HTMLHtmlElement to the main dom.h includes

--- include/dom/dom.h
+++ include/dom/dom.h
@@ -38,8 +38,9 @@
 #include <dom/core/comment.h>
 
 /* DOM HTML headers */
-#include <dom/html/html_element.h>
 #include <dom/html/html_document.h>
+#include <dom/html/html_element.h>
+#include <dom/html/html_html_element.h>
 #include <dom/html/html_form_element.h>
 
 /* DOM Events header */
