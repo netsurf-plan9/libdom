ce28e10c6c8018380b0609b715f59f8fd1550a21
rsk1994 <rsk1coder99@gmail.com>
2014-05-09 20:47:13 +0530

    Label Element

--- src/html/html_document.c
+++ src/html/html_document.c
@@ -35,6 +35,7 @@
 #include "html/html_quote_element.h"
 #include "html/html_pre_element.h"
 #include "html/html_br_element.h"
+#include "html/html_label_element.h"
 
 #include "core/attr.h"
 #include "core/string.h"
@@ -270,6 +271,9 @@ _dom_html_document_create_element_internal(dom_html_document *html,
 	} else if (dom_string_caseless_isequal(tag_name, html->memoised[hds_BR])) {
 		exc = _dom_html_br_element_create(html, namespace, prefix,
 				(dom_html_br_element **) result);
+	} else if (dom_string_caseless_isequal(tag_name, html->memoised[hds_LABEL])) {
+		exc = _dom_html_label_element_create(html, namespace, prefix,
+				(dom_html_label_element **) result);
 	} else {
 		exc =  _dom_html_element_create(html, tag_name, namespace,
 						prefix, result);
