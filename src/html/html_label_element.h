07b7dd8f5f102cf6f6e2e492bf80f9a9eb798738
Michael Drake <michael.drake@codethink.co.uk>
2016-02-05 16:53:38 +0000

    Fix: Proper copy constructor for HTMLLabelElement.

--- src/html/html_label_element.h
+++ src/html/html_label_element.h
@@ -49,4 +49,13 @@ dom_exception _dom_html_label_element_copy(dom_node_internal *old,
 	_dom_virtual_html_label_element_destroy, \
 	_dom_html_label_element_copy
 
+/* Helper functions*/
+dom_exception _dom_html_label_element_copy_internal(
+		dom_html_label_element *old,
+		dom_html_label_element *new);
+#define dom_html_label_element_copy_internal(o, n) \
+		_dom_html_label_element_copy_internal( \
+				(dom_html_label_element *) (o), \
+				(dom_html_label_element *) (n))
+
 #endif
