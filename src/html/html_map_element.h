c02718070450fb819b42b011b1b083b83a0b2bdf
Michael Drake <michael.drake@codethink.co.uk>
2016-02-05 17:01:31 +0000

    Fix: Proper copy constructor for HTMLMapElement.

--- src/html/html_map_element.h
+++ src/html/html_map_element.h
@@ -49,6 +49,15 @@ dom_exception _dom_html_map_element_copy(dom_node_internal *old,
 	_dom_virtual_html_map_element_destroy, \
 	_dom_html_map_element_copy
 
+/* Helper functions*/
+dom_exception _dom_html_map_element_copy_internal(
+		dom_html_map_element *old,
+		dom_html_map_element *new);
+#define dom_html_map_element_copy_internal(o, n) \
+		_dom_html_map_element_copy_internal( \
+				(dom_html_map_element *) (o), \
+				(dom_html_map_element *) (n))
+
 #endif
 
 bool callback(struct dom_node_internal *node, void *ctx);
