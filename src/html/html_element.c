89f73a033cd3398b1816903a6a0714d3aacee524
Michael Drake <michael.drake@codethink.co.uk>
2016-02-05 15:19:50 +0000

    Fix: Implement HTMLElement copy constructor.
    It can't just use the Element copy constructor, as there is aditional
    stuff in the HTMLElement struct.

--- src/html/html_element.c
+++ src/html/html_element.c
@@ -101,7 +101,37 @@ void _dom_html_element_destroy(dom_node_internal *node)
 dom_exception _dom_html_element_copy(dom_node_internal *old,
 		dom_node_internal **copy)
 {
-	return _dom_element_copy(old, copy);
+	dom_html_element *new_node;
+	dom_exception err;
+
+	new_node = malloc(sizeof(dom_html_element));
+	if (new_node == NULL)
+		return DOM_NO_MEM_ERR;
+
+	err = dom_html_element_copy_internal(old, new_node);
+	if (err != DOM_NO_ERR) {
+		free(new_node);
+		return err;
+	}
+
+	*copy = (dom_node_internal *) new_node;
+
+	return DOM_NO_ERR;
+}
+
+dom_exception _dom_html_element_copy_internal(
+		dom_html_element *old, dom_html_element *new)
+{
+	dom_exception err;
+
+	err = dom_element_copy_internal(old, new);
+	if (err != DOM_NO_ERR) {
+		return err;
+	}
+
+	new->type = old->type;
+
+	return DOM_NO_ERR;
 }
 
 /*-----------------------------------------------------------------------*/
